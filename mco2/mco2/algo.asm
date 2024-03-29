58ba3a94f022af4c49c9a18fe8b196d164a209c2
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2024-03-30 00:04:15 +0800

    made new algorithm for double precision
    I think this works for any sizes just try it out with a larger data set

--- mco2/mco2/algo.asm
+++ mco2/mco2/algo.asm
@@ -1,22 +1,37 @@
 %include "io64.inc"
 section .data 
 ;test data here but this should get input from C...I think
-a dd 2.0, 2.0, 2.0, 2.0
-b dd 2.0, 4.0, 6.0, 8.0
+a dq 2.0, 2.0, 2.0, 2.0
+b dq 2.0, 4.0, 6.0, 8.0
 n dd 4 ;length
-sdot dd 0.0 ;total
+sdot dq 0.0 ;total
 section .text
 global main
 main:
     mov rbp, rsp; for correct debugging
     ;write your code here
     
-                 movups xmm1, [a]
-		movups xmm2, [b]
-		mulps xmm1, xmm2
-		haddps xmm1, xmm1
-		haddps xmm1, xmm1
-                 movups [sdot], xmm1
+    mov rsi, a
+    mov rdi, b
+    mov rcx, [n]
+    
+    xorpd xmm0, xmm0
+    
+    NEXT: 
+        movapd xmm1, [rsi]
+        movapd xmm2, [rdi]
+        
+        mulpd xmm1, xmm2
+        haddpd  xmm1, xmm1
+        addpd xmm0, xmm1
+        
+        add rdi, 16
+        add rsi, 16
+        
+        loop NEXT
+        
+        movapd [sdot], xmm0
+
                 
     xor rax, rax
     ret
