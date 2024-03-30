207b49c979e6d7cf7f61abe397a0ecc103c36c83
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2024-03-30 16:04:23 +0800

    randome number

--- mco2/mco2/kernel.c
+++ mco2/mco2/kernel.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
+#include <time.h>
+
+
+double randomDouble(double min, double max) {
+	return ((double)rand() / RAND_MAX) * (max - min) + min;
+}
 
 double dotProdC(double* a, double* b, int n) {
 	double sdot = 0.0; //store final answer
@@ -11,7 +17,7 @@ double dotProdC(double* a, double* b, int n) {
 	return sdot;
 }
 
-//extern void asmhello(); CALL THE ASM FUNCITION HERE
+//extern void asmhello();  EXTERN CALL HERE
 
 int main() {
 	int n = 8; //vector length
@@ -20,30 +26,27 @@ int main() {
 	//double b[n] = {};
 	double *b = malloc(n * sizeof(double));
 
+	for (int i = 0; i < n; i++) {
+		a[i] = 2.0;
+	};
 
-	a[0] = 2.0;
-	a[1] = 2.0;
-	a[2] = 2.0;
-	a[3] = 2.0;
-	a[4] = 2.0;
-	a[5] = 2.0;
-	a[6] = 2.0;
-	a[7] = 2.0;
+	for (int i = 0; i < n; i++) {
+		b[i] = randomDouble(0.0, 10.0); 
+	}
 
-	b[0] = 2.0;
-	b[1] = 4.0;
-	b[2] = 6.0;
-	b[3] = 8.0;
-	b[4] = 10.0;
-	b[5] = 12.0;
-	b[6] = 14.0;
-	b[7] = 16.0;
+	for (int i = 0; i < n; i++) {
+		printf("%lf ", a[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < n; i++) {
+		printf("%lf ", b[i]);
+	}
+	printf("\n");
 
-	// asmhello();  INSERT  ASSEMBLY FUNCTION HERE
 	double kernelC = dotProdC(a, b, n);
-	printf("%lf", kernelC);
+	printf("\n%lf", kernelC);
 
-	//double kernelAsm = dotProdAsm(a, b, n);
+	//double kernelAsm = dotProdAsm(a, b, n); INSERT ASSEMBLY CALL HERE
 	
 	return 0;
 }
