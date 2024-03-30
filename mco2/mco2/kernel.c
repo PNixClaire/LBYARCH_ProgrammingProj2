4346d09f83de9bcb913baf743520f17f2d701ba5
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2024-03-30 15:25:10 +0800

    current status

--- mco2/mco2/kernel.c
+++ mco2/mco2/kernel.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
 
 double dotProdC(double* a, double* b, int n) {
 	double sdot = 0.0; //store final answer
@@ -10,20 +11,39 @@ double dotProdC(double* a, double* b, int n) {
 	return sdot;
 }
 
-extern double dotProd(double* a, double* b, int n);
+//extern void asmhello(); CALL THE ASM FUNCITION HERE
 
-//C program to call both kernels 
 int main() {
+	int n = 8; //vector length
+	//double a[n] = {};
+	double *a = malloc(n * sizeof(double));
+	//double b[n] = {};
+	double *b = malloc(n * sizeof(double));
 
-	//might do something so we can set the length and then randomize the elements
-	int n = 0; //vector length
-	double a[] = {};
-	double b[] = {};
 
+	a[0] = 2.0;
+	a[1] = 2.0;
+	a[2] = 2.0;
+	a[3] = 2.0;
+	a[4] = 2.0;
+	a[5] = 2.0;
+	a[6] = 2.0;
+	a[7] = 2.0;
+
+	b[0] = 2.0;
+	b[1] = 4.0;
+	b[2] = 6.0;
+	b[3] = 8.0;
+	b[4] = 10.0;
+	b[5] = 12.0;
+	b[6] = 14.0;
+	b[7] = 16.0;
+
+	// asmhello();  INSERT  ASSEMBLY FUNCTION HERE
 	double kernelC = dotProdC(a, b, n);
+	printf("%lf", kernelC);
 
-	double kernelAsm = dotProdAsm(a, b, n);
-
-
+	//double kernelAsm = dotProdAsm(a, b, n);
+	
 	return 0;
 }
