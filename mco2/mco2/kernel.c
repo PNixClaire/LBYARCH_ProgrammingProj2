c9e0c155f6d92416da5b34af3df1df51fa8575b8
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2024-03-31 20:34:10 +0800

    Added correctness output and removed printing of variables

--- mco2/mco2/kernel.c
+++ mco2/mco2/kernel.c
@@ -17,41 +17,31 @@ double dotProdC(double* a, double* b, int n) {
 	return sdot;
 }
 
-extern double dotProdAsm(double *a, double* b,int n);  //EXTERN CALL HERE
+extern double dotProdAsm(double* a, double* b, int n);  //EXTERN CALL HERE
 
 int main() {
-	int n = 100000; //vector length
-	//double a[n] = {};
-	double *a = (double*) malloc(n * sizeof(double));
-	//double b[n] = {};
-	double *b = (double*)malloc(n * sizeof(double));
-	int kernel; 
-	
+	int n = 67108864; //vector length
+	double* a = (double*)malloc(n * sizeof(double));
+	double* b = (double*)malloc(n * sizeof(double));
+	int kernel;
+
 	for (int i = 0; i < n; i++) {
 		a[i] = 2.0;
 	};
 
 	for (int i = 0; i < n; i++) {
-		b[i] = randomDouble(0.0, 10.0); 
+		b[i] = randomDouble(0.0, 10.0);
 	}
-
-	for (int i = 0; i < n; i++) {
-		printf("%lf ", a[i]);
-	}
-	printf("\n");
-	for (int i = 0; i < n; i++) {
-		printf("%lf ", b[i]);
-	}
-	printf("\n");
 
 	//prompt 
+	
 	printf("[1] Kernel.c [2] Kernel.asm \n Choose a Kernel to run: ");
 	scanf_s("%d", &kernel);
 
 	if (kernel == 1) {
 		clock_t start = clock();
 		double kernelC = dotProdC(a, b, n);
-		clock_t end = clock();
+		clock_t end = clock(); 
 		printf("\n%lf", kernelC);
 		printf("\nRuntime: %lf", (double)(end - start));
 	}
@@ -68,6 +58,7 @@ int main() {
 	
 
 	//to test if they have the same answer
+
 	/*double kernelC = dotProdC(a, b, n);
 	printf("\n%lf", kernelC);
 
@@ -76,6 +67,6 @@ int main() {
 
 	free(a);
 	free(b);
-	
+
 	return 0;
 }
